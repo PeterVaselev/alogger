f8ba84441f84de3dbfd0f4a5bdd93a1f27a2b0bd
Peter Vaselev <petervaselev@gmail.com>
2025-10-26 22:16:23 +0300

    Реализовал run в новом потоке. Есть проблемы описанные в todo.

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,13 +1,18 @@
 #include "Logger.hpp"
 #include <ios>
+#include <stdexcept>
 #include <string>
 #include <thread>
-
+#include <mutex>
+#include <format>
+#include <chrono>
 
 Logger::Logger() : _file("log.txt", std::ios::app){
     if (!_file.is_open()){
         throw std::runtime_error("Failed to open file logs.txt for logs");
     }
+
+    this->run();
 }
 
 
@@ -15,16 +20,76 @@ Logger::Logger(const std::string& file_name) : _file(file_name, std::ios::app){
     if (!_file.is_open()){
         throw std::runtime_error("Failed to open file " + file_name + " for logs");
     }
+    this->run();
+}
+
+void Logger::write_log(Log log){
+    const auto now = std::chrono::system_clock::now(); 
+    std::string formatted_time = std::format("{:%d.%m.%Y %H:%M:%S}", now);
+    
+    switch(log.level){
+        case Logger::LogLevel::ERROR:
+            this->_file << formatted_time << "\tERROR\t" << log.log << std::endl;
+            break;
+
+        case Logger::LogLevel::WARNING:
+            this->_file << formatted_time << "\tWARN\t" << log.log << std::endl;
+            break;
+
+        case Logger::LogLevel::INFO:
+            this->_file << formatted_time << "\tINFO\t" << log.log << std::endl;
+            break;
+
+        default:
+            this->_file << formatted_time << "\tNONE\t" << log.log << std::endl;
+            break;
+    }
 }
 
 
-auto Logger::Log::operator<=>(const Log& other){
-    return this->level <=> other.level;
-}
+//TODO: Здесь может быть гонка данных, необходимо переписать так, чтобы все последние данные успели обработаться
+void Logger::run(){
+    worker_run = true;
 
-
-void run(){
-    std::thread Writer([]() {
+    this->worker = std::thread([this]() {
         
+        std::unique_lock<std::mutex> lock (this->mtx);
+        while (this->worker_run){
+            cv.wait(lock, [this](){
+                return !this->_queue.empty() or !worker_run;
+            });
+
+            if (!this->_queue.empty()){
+                std::queue<Logger::Log> local_queue;
+                this->_queue.swap(local_queue);
+                lock.unlock();
+
+                while (!local_queue.empty()){
+                    auto log = local_queue.front();
+                    local_queue.pop();
+                    this->write_log(log);
+                }
+
+            }
+            lock.lock();
+
+        }
+        if (!this->_queue.empty()){
+            std::queue<Logger::Log> local_queue;
+            this->_queue.swap(local_queue);
+            lock.unlock();
+            while(!local_queue.empty()){
+                this->write_log(local_queue.front());
+                local_queue.pop();
+            }
+        }
     });
+
+
+}
+
+Logger::~Logger(){
+    worker_run = false;
+    cv.notify_one();
+    worker.join();
 }
--- src/Logger.hpp
+++ src/Logger.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
-
+#include <condition_variable>
 #include <fstream>
-#include <memory>
 #include <mutex>
 #include <queue>
 #include <string>
+
+
+//TODO: подумать про вариативное использование priority_queue для различных кейсов
+//Так же сделать вариант исопльзования собственных format для времени и даты
+//Так же сделать дополнительное поле TAG для более информативных логов
 
 class Logger {
 private:
@@ -19,23 +23,32 @@ private:
         LogLevel level;
         std::string log;
 
-        auto operator<=> (const Log& other);
     };
 
-
+    
     std::ofstream _file;
     
-    std::priority_queue<Log> _queue;
+    std::queue<Log> _queue;
+
+    std::condition_variable cv;
 
     std::mutex mtx;
+
+    std::thread worker;
+    bool worker_run;
+
+    void write_log(Log log);
+
+    void run();
+
 
 public:
 
     Logger();
     Logger(const std::string& file_name);
 
-    void run();
-
+    //TODO: проверить на оптимизацию с rvalue lvalue и текущим вариантом. 
+    //По предположению в данном случае компилятор сам выберет оптимальный вариант
     void logi(std::string log);
     void logw(std::string log);
     void loge(std::string log);
